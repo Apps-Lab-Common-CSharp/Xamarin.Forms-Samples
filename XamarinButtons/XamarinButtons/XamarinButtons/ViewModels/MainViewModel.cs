4f2a0cfb8510786b5f5d90c4aa03b80d239b3a7d
Peter Milovcik <peter.milovcik@gmail.com>
2020-01-21 15:33:21 +0100

    Initial version

---
+++ XamarinButtons/XamarinButtons/XamarinButtons/ViewModels/MainViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+
+namespace XamarinButtons.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string text;
+
+        public MainViewModel()
+        {
+            ButtonCommand = new Command<string>(Button);
+        }
+
+        public string Text
+        {
+            get => text;
+            set
+            {
+                if (Equals(text, value)) return;
+                text = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Command<string> ButtonCommand { get; }
+
+        private void Button(string parameter)
+        {
+            Text = parameter;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
